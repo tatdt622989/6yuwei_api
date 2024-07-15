36a4d5175a3d4bca3c71ac0dbbca27e4088f532d
tatdt622989 <70212cbkj67@gmail.com>
2024-07-15 20:34:53 +0800

    Separate the behaviors generated by guesssai-canvas and put them into the controller

--- controllers/guessai_canvas.js
+++ controllers/guessai_canvas.js
@@ -10,6 +10,9 @@ const openai = new OpenAI({
   apiKey: OpenAIAPIKey,
 });
 
+const socketState = require('./socketState');
+const state = socketState.getSocketState();
+
 const apiDomain = process.env.API_DOMAIN;
 
 // model
@@ -342,6 +345,196 @@ const getCanvasList = async (req, res) => {
   });
 };
 
+const generateCanvas = async () => {
+  console.log('generate canvas');
+  if (state.isCanvasGenerating) {
+    return;
+  }
+  state.isCanvasGenerating = true;
+  let content = null;
+
+  // get theme from db
+  const themeCount = await Theme.countDocuments();
+  const random = Math.floor(Math.random() * themeCount);
+  const theme = await Theme.findOne().skip(random);
+
+  // generate canvas
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4o',
+      messages: [
+        {
+          role: 'system',
+          content: 'As an HTML canvas expert and a code artist specializing in theme-based creations, with a keen emphasis on both the aesthetic intricacies and the security of the generated code, I strive to bring forth a unique fusion of technology and artistry.',
+        },
+        {
+          role: 'user',
+          content: `
+          Create HTML canvas code with '${theme.themeEN}' as the theme, and return it in JSON format within the constraint of 5000 characters.
+          Emphasize the need for the drawing to be as intricate and lifelike as possible.Do not omit.Do not include any image URLs, or JavaScript comments in the code.`,
+        },
+      ],
+      temperature: 1,
+      tools: [
+        {
+          type: 'function',
+          function: {
+            name: 'canvasDraw',
+            description: 'Generate a canvas image with a 16:9 aspect ratio using only the <script> and <canvas> tags in HTML. Do not include any JavaScript comments in the code.',
+            parameters: {
+              type: 'object',
+              properties: {
+                code: {
+                  type: 'string',
+                  description: 'HTML canvas Tag with inline javascript',
+                },
+              },
+              required: ['code'],
+            },
+          },
+        },
+      ],
+      tool_choice: { type: 'function', function: { name: 'canvasDraw' } },
+    });
+
+    content = response.choices[0]?.message?.tool_calls[0]?.function.arguments;
+    console.log('content', content);
+    if (!content
+      || !content.code) {
+      state.isCanvasGenerating = false;
+      // retry
+      // generateCanvas();
+    }
+    content = JSON.parse(content);
+  } catch (err) {
+    console.log(err);
+    state.isCanvasGenerating = false;
+    // retry
+    // generateCanvas();
+  }
+
+  // If the code contains an answer, remove it as a random English word.
+  // If a reserved word does not replace
+  const reservedWords = [
+    'abstract',
+    'arguments',
+    'await',
+    'boolean',
+    'break',
+    'byte',
+    'case',
+    'catch',
+    'char',
+    'class',
+    'const',
+    'continue',
+    'debugger',
+    'default',
+    'delete',
+    'do',
+    'double',
+    'else',
+    'enum',
+    'eval',
+    'export',
+    'extends',
+    'false',
+    'final',
+    'finally',
+    'float',
+    'for',
+    'function',
+    'goto',
+    'if',
+    'implements',
+    'import',
+    'in',
+    'instanceof',
+    'int',
+    'interface',
+    'let',
+    'long',
+    'native',
+    'new',
+    'null',
+    'package',
+    'private',
+    'protected',
+    'public',
+    'return',
+    'short',
+    'static',
+    'super',
+    'switch',
+    'synchronized',
+    'this',
+    'throw',
+    'throws',
+    'transient',
+    'true',
+    'try',
+    'typeof',
+    'var',
+    'void',
+    'volatile',
+    'while',
+    'with',
+    'yield',
+    'Math',
+    'Array',
+    'Date',
+  ];
+  if (!reservedWords.includes(theme.themeEN.toLowerCase())) {
+    const atoz = 'abcdefghijklmnopqrstuvwxyz';
+    const wordLength = 5;
+    let randomEN = '';
+    for (let i = 0; i < wordLength; i += 1) {
+      randomEN += atoz[Math.floor(Math.random() * atoz.length)];
+    }
+    const answers = [
+      theme.themeEN, // original
+      theme.themeEN.toLowerCase(), // lowercase
+      theme.themeEN.toLowerCase().charAt(0).toUpperCase()
+       + theme.themeEN.toLowerCase().slice(1), // capitalize first letter
+      theme.themeEN.toUpperCase(), // capitalize all letters
+      theme.themeEN.toLowerCase().replace(' ', '-'), // dash
+      theme.themeEN.toLowerCase().replace(' ', '_'), // underscore
+      theme.themeEN.toLowerCase().replace(' ', ''), // remove space
+      theme.themeEN.toLowerCase().replace(' ', '').charAt(0).toUpperCase() + theme.themeEN.toLowerCase().replace(' ', '').slice(1), // remove space and capitalize first letter
+    ];
+    for (let i = 0; i < answers.length; i += 1) {
+      content.code = content.code.replace(new RegExp(answers[i], 'g'), randomEN);
+    }
+  }
+
+  // save canvas to db
+  const {
+    code,
+  } = content;
+
+  try {
+    const guessaiCanvas = new GuessAICanvas({
+      canvas: code,
+      answerTW: theme.themeTW,
+      answerEN: theme.themeEN,
+      answerJP: theme.themeJP,
+      solved: false,
+    });
+    await guessaiCanvas.save();
+  } catch (err) {
+    console.log(err);
+  }
+
+  // emit canvas to all clients
+  io.emit('server canvas', {
+    status: 'done',
+  });
+
+  console.log('generate canvas done');
+  state.isCanvasGenerating = false;
+};
+
 module.exports = {
   createSimpleUser,
   createTheme,
@@ -354,4 +547,5 @@ module.exports = {
   getMsgList,
   getRanking,
   getGalleryCanvas,
+  generateCanvas,
 };
